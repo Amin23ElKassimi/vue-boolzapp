5f0dea41c9b18eab632f46f6f1cd4e2c725c4b45
Amin23ElKassimi <aminelkassimi01@gmail.com>
2023-12-06 11:46:27 +0100

    Milestone 01 Completed

--- js/script.js
+++ js/script.js
@@ -5,7 +5,24 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            contacts: [
+                {name: 'Luigi', image: 'img/avatar_1.jpg'},
+                {name: 'Amin', image: 'img/avatar_2.jpg'},
+                {name: 'Susan', image: 'img/avatar_3.jpg'},
+                {name: 'Marta', image: 'img/avatar_4.jpg'},
+                {name: 'Jessica', image: 'img/avatar_5.jpg'},
+                {name: 'Imane', image: 'img/avatar_6.jpg'},
+                {name: 'Federico', image: 'img/avatar_7.jpg'},
+                {name: 'Davide', image: 'img/avatar_8.jpg'},
+                {name: 'Alessandro', image: 'img/avatar_1.jpg'},
+                {name: 'Federico', image: 'img/avatar_2.jpg'},
+                {name: 'Guglielmo', image: 'img/avatar_3.jpg'},
+                {name: 'Simone', image: 'img/avatar_4.jpg'},
+                {name: 'Chiara', image: 'img/avatar_5.jpg'},
+                {name: 'Imane', image: 'img/avatar_6.jpg'},
+                {name: 'Federico', image: 'img/avatar_7.jpg'},
+                {name: 'Davide', image: 'img/avatar_8.jpg'}
+            ]    
         }
     },
     methods: {
