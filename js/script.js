60160258094d0338cbcff986f34534f4d67af1a5
Amin23ElKassimi <aminelkassimi01@gmail.com>
2023-12-06 12:26:03 +0100

    Update array with new object: messages

--- js/script.js
+++ js/script.js
@@ -6,24 +6,237 @@ createApp({
     data() {
         return {
             contacts: [
-                {name: 'Luigi', image: 'img/avatar_1.jpg'},
-                {name: 'Amin', image: 'img/avatar_2.jpg'},
-                {name: 'Susan', image: 'img/avatar_3.jpg'},
-                {name: 'Marta', image: 'img/avatar_4.jpg'},
-                {name: 'Jessica', image: 'img/avatar_5.jpg'},
-                {name: 'Imane', image: 'img/avatar_6.jpg'},
-                {name: 'Federico', image: 'img/avatar_7.jpg'},
-                {name: 'Davide', image: 'img/avatar_8.jpg'},
-                {name: 'Alessandro', image: 'img/avatar_1.jpg'},
-                {name: 'Federico', image: 'img/avatar_2.jpg'},
-                {name: 'Guglielmo', image: 'img/avatar_3.jpg'},
-                {name: 'Simone', image: 'img/avatar_4.jpg'},
-                {name: 'Chiara', image: 'img/avatar_5.jpg'},
-                {name: 'Imane', image: 'img/avatar_6.jpg'},
-                {name: 'Federico', image: 'img/avatar_7.jpg'},
-                {name: 'Davide', image: 'img/avatar_8.jpg'}
-            ]    
-        }
+                    {   name: 'Fatima', image: 'https://i.pravatar.cc/43?img=47',
+                        visible: true,
+                        messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]},
+                    {   name: 'Amin', image: 'img/avatar_2.jpg',
+                        visible: true,
+                        messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]},
+                    {   name: 'Federico', image: 'img/avatar_3.jpg',
+                    visible: true,
+                    messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                    ]},  
+                    {   name: 'Zoe', image: 'https://i.pravatar.cc/47?img=40',
+                        visible: true,
+                        messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]},
+                    {   name: 'Jessica', image: 'img/avatar_4.jpg',
+                    visible: true,
+                    messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                    ]},
+                    {   name: 'Alessandro', image: 'img/avatar_5.jpg',
+                        visible: true,
+                        messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]},                    
+                    {   name: 'Annet', image: 'https://i.pravatar.cc/49?img=49',
+                    visible: true,
+                    messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                    ]},
+                    {   name: 'Susan', image: 'img/avatar_6.jpg',
+                    visible: true,
+                    messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                    ]},
+                    {   name: 'Mike', image: 'img/avatar_7.jpg',
+                    visible: true,
+                    messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                    ]},
+                    {   name: 'Saber', image: 'img/avatar_8.jpg',
+                        visible: true,
+                        messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]},
+                    {   name: 'Katerin', image: 'https://i.pravatar.cc/150?img=43',
+                    visible: true,
+                    messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                    ]},                    
+
+                    {   name: 'Adele', image: 'https://i.pravatar.cc/43?img=29',
+                    visible: true,
+                    messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                    ]},
+                ]    
+             }   
     },
     methods: {
         removeTask(index){
